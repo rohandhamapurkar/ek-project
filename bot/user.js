b4c8435eaf04f05aafa1241d8e34f5e2e08e9a07
786kaizersoze <dhamapurkar54@gmail.com>
2018-04-22 16:50:48 +0530

    debugging

--- bot/user.js
+++ bot/user.js
@@ -3,7 +3,7 @@ var controller = require('./init-user').controller;
 const apiaibotkit = require('api-ai-botkit');
 const axios = require('axios');
 const apiai = apiaibotkit("216dd6479acc4c63a223fc33b034eefb");
-
+let a = false;
 controller.hears('.*','message_received',function(bot,message){
     if (message.type === 'message_received') {
         apiai.process(message, bot);
@@ -13,9 +13,9 @@ apiai.all(function (message, resp, bot) {
     console.log(message);
     console.log(resp);
     bot.reply(message, resp.result.fulfillment.speech);
-    let a = false;
     if(!a){
-        apiai.process({text:"family_introvert"},bot);
+        message.text = "family_introvert"
+        apiai.process(message,bot);
         a = true;
     }
 })
