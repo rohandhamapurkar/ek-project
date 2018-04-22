b1b91a79df4605e40efb910e455603b7e2cfd693
786kaizersoze <dhamapurkar54@gmail.com>
2018-04-22 16:17:15 +0530

    changed port

--- bot/env.js
+++ bot/env.js
@@ -1,5 +1,5 @@
 module.exports = {
     ACCESS_TOKEN :"EAADwlFkaTB4BAP4lJtpirBL4byiEaMvmz6EVVNRgeerTx8WnMZBNy8mdOgMZBOe5ZA4KbSsCqjbSGlSo47ehTeya5quTh76rZCK9baZAjayNMYOEWrpQfNQfaYIeZCqUqR50QOrBNLDeIjYSjDfUvsE4B7VASFlu8HrOJbZBuC6rgZDZD",
     VERIFY_TOKEN : 'misalpavbro',
-    PORT : 4026
+    PORT : 3012
 }
