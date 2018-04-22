72b9e9969662603df1c5f56e376fd5e6bf6e2e5e
786kaizersoze <dhamapurkar54@gmail.com>
2018-04-22 16:22:53 +0530

    debug

--- bot/user.js
+++ bot/user.js
@@ -10,7 +10,7 @@ controller.hears('.*','message_received',function(bot,message){
 });
 apiai.all(function (message, resp, bot) {
     console.log(message);
-    console.log(resp.result.action);
+    console.log(resp);
     bot.reply(message, resp.queryResult.fulfillmentText);
 })
 require("./conversations/familyIntrovert")(controller,bot,apiai);
