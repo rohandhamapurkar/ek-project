64704e9247dd73fd8714a66d11b45587c84a9874
786kaizersoze <dhamapurkar54@gmail.com>
2018-04-22 16:48:52 +0530

    debugging

--- bot/user.js
+++ bot/user.js
@@ -13,17 +13,11 @@ apiai.all(function (message, resp, bot) {
     console.log(message);
     console.log(resp);
     bot.reply(message, resp.result.fulfillment.speech);
-    axios.get('https://api.dialogflow.com/v1/query?v=20150910&e=family_introvert&timezone=Asia&lang=en&sessionId=1234567890',
-    {
-        headers:{
-            Authorization: "Bearer 216dd6479acc4c63a223fc33b034eefb"
-        }}).then(function (response) {
-            console.log(response);
-            bot.reply(message, response.data.result.fulfillment.speech);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+    let a = false;
+    if(!a){
+        apiai.process({text:"family_introvert"},bot);
+        a = true;
+    }
 })
 require("./conversations/familyIntrovert")(controller,bot,apiai);
 
